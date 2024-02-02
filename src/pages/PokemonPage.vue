0b86fdf1b8122a2aecf67399e02064fa35335306
@FranCarn <carnevalefranco99@gmail.com>
2024-02-02 19:51:13 -0300

    page skeleton ready

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Who's that pokemon?</h1>
+
+    <!-- TODO: Picture -->
+    <PokemonPicture />
+    <!-- TODO: Options -->
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions";
+import PokemonPicture from "@/components/PokemonPicture";
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+};
+</script>
